d63bb12a19ccad20e5a074c9f6b77e6e109f7bf5
Nikita Mikhailov <nikitamikhaylov@clickhouse.com>
2025-03-11 14:55:57 +0100

    Better

--- src/Interpreters/Context.h
+++ src/Interpreters/Context.h
@@ -1301,7 +1301,6 @@ public:
     {
         SERVER,         /// The program is run as clickhouse-server daemon (default behavior)
         CLIENT,         /// clickhouse-client
-        EMBEDDED_CLIENT,/// clickhouse-client being run over SSH tunnel
         LOCAL,          /// clickhouse-local
         KEEPER,         /// clickhouse-keeper (also daemon)
         DISKS,          /// clickhouse-disks
